0d7bd1922d73cbe32d5e40c054fdb5dbe05f4adb
Lucas de Freitas Kauer <lucas.kauerrs@hotmail.com>
2015-06-02 15:03:59 -0300

    add(TurboRango) - Adicionando comentários.

--- src/TurboRango/TurboRango.Dominio/Restaurante.cs
+++ src/TurboRango/TurboRango.Dominio/Restaurante.cs
@@ -8,7 +8,11 @@ namespace TurboRango.Dominio
 {
     internal class Restaurante
     {
+        /// <summary>
+        /// Capacidade (lotação máxima) do restaurante
+        /// </summary>
         internal int Capacidade { get; set; }
+        // Int32 or Int64
         internal string Nome { get; set; }
         internal Localizacao Localizacao { get; set; }
         internal Contato Contato { get; set; }
