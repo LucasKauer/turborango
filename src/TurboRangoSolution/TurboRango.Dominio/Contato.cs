f1599e670a54af53622cf91a6a1496d0f58e74a2
Lucas de Freitas Kauer <lucas.kauerrs@hotmail.com>
2015-06-02 13:54:46 -0300

    create(TurboRangoSolution) - Subindo o tema.

---
+++ src/TurboRangoSolution/TurboRango.Dominio/Contato.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TurboRango.Dominio
+{
+    internal class Contato
+    {
+        internal string site;
+        internal string telefone;
+    }
+}
---
+++ src/TurboRangoSolution/TurboRango.Dominio/Localizacao.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TurboRango.Dominio
+{
+    internal class Localizacao
+    {
+        internal string bairro;
+        internal int latitude;
+        internal string logradouro;
+        internal int longitude;
+    }
+}
