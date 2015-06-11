e55ebb7742fb5d4615e6a54ac7135c396084db9c
Lucas de Freitas Kauer <lucas.kauerrs@hotmail.com>
2015-06-11 16:59:52 -0300

    feat(TurboRango) - Resolvendo problemas com merge

--- src/TurboRango/TurboRango.Web/Models/IdentityModels.cs
+++ src/TurboRango/TurboRango.Web/Models/IdentityModels.cs
@@ -1,8 +1,8 @@
-﻿using System.Data.Entity;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
 using System.Security.Claims;
 using System.Threading.Tasks;
-using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.EntityFramework;
 using TurboRango.Dominio;
 
 namespace TurboRango.Web.Models
@@ -37,7 +37,5 @@ namespace TurboRango.Web.Models
         public DbSet<Contato> Contatos { get; set; }
 
         public DbSet<Localizacao> Localizacoes { get; set; }
-
-        public DbSet<Cardapio> Cardapios { get; set; }
     }
 }
