5dadca072e48c28ba834c8c9af500832d28c072f
Lucas de Freitas Kauer <lucas.kauerrs@hotmail.com>
2015-06-11 00:24:28 -0300

    feat(TurboRango) - Separando o projeto por pastas. - Criando uma Controller específica para lidar com os cardápios e uma view sem funcionalidades.

---
+++ src/TurboRango/TurboRango.Web/Controllers/Restaurantes/CardapioController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TurboRango.Web.Controllers
+{
+    public class CardapioController : Controller
+    {
+        // GET: Cardapio
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
