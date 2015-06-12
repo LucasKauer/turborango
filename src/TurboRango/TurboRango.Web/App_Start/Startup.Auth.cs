2538c36638da1789e44f0da9f1717dad69d59dd9
Lucas de Freitas Kauer <lucas.kauerrs@hotmail.com>
2015-06-12 02:38:13 -0300

    feat(Update) - Colocando as alterações que foram feitas no projeto do @Bernardo

--- src/TurboRango/TurboRango.Web/App_Start/Startup.Auth.cs
+++ src/TurboRango/TurboRango.Web/App_Start/Startup.Auth.cs
@@ -24,6 +24,7 @@ namespace TurboRango.Web
             // Configure the sign in cookie
             app.UseCookieAuthentication(new CookieAuthenticationOptions
             {
+                ExpireTimeSpan = TimeSpan.FromMinutes(5),
                 AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
                 LoginPath = new PathString("/Account/Login"),
                 Provider = new CookieAuthenticationProvider
